35fc6fc571881ac6f4a69b848ec0d9d317b28d4f
Askhat Shaikhin <shaikhinab@gmail.com>
2022-07-21 21:43:12 +0600

    added configs of swagger and bearer token

--- OnlineStoreApi/Controllers/ProductController.cs
+++ OnlineStoreApi/Controllers/ProductController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OnlineStore.Model;
 using Services.Interfaces;
 
 namespace OnlineStoreApi.Controllers
 {
+    [Authorize(AuthenticationSchemes = "Bearer", Roles = "clientsUser")]
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
@@ -31,7 +33,10 @@ namespace OnlineStoreApi.Controllers
         [HttpPut("createProduct")]
         public async Task<IActionResult> CreateProduct([FromBody] Product product)
         {
-            throw new NotImplementedException();
+            _onlineStoreContext.Add(product);
+            await _onlineStoreContext.SaveChangesAsync();
+
+            return Ok(product);
         }
 
         [HttpPost("updateProduct")]
--- OnlineStoreApi/Program.cs
+++ OnlineStoreApi/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OnlineStore.Repo.Impl;
 using OnlineStore.Repo.Interfaces;
 using OnlineStoreApi;
 using Services.Impl;
 using Services.Interfaces;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +15,51 @@ var connString = builder.Configuration.GetConnectionString("OnlineStoreDb");
 
 builder.Services.AddDbContext<OnlineStoreContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("OnlineStoreDb"), b=>b.MigrationsAssembly("OnlineStoreApi")));
 
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer();
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(
+                options =>
+                {
+                    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Description = "Please insert JWT with Bearer into field",
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey
+                    });
+
+                    options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                        },
+                        new string[] { }
+                    }
+                    });
+                });
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = "https://localhost:7062",
+                        ValidAudience = "https://localhost:7062",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("HSFVBHASTGSHDVASDS"))
+                    };
+                });
 
 builder.Services.AddTransient<IProductInterface, ProductService>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
